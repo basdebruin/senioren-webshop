49b7cc97787d97e494fb56e7f15c9ffc98841ba7
Bas de Bruin <bruin998@gmail.com>
2023-08-27 14:45:36 +0200

    styling for ImageGallery component

--- src/routes/products/wasmachines/samsung/ImageGallery.svelte
+++ src/routes/products/wasmachines/samsung/ImageGallery.svelte
@@ -25,3 +25,52 @@
         <i class="ri-arrow-right-s-line"></i>
     </button>
 </div>
+
+<style lang="scss">
+    .image-gallery {
+        position: relative;
+        display: block;
+        width: 100%;
+
+        .main-image {
+            margin: 1rem auto;
+            width: 60%;
+            height: 400px;
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+            }
+        }
+
+        .thumbnails {
+            display: flex;
+            margin: 1rem auto;
+            width: 100%;
+            gap: .5rem;
+            justify-content: center;
+
+            .thumbnail {
+                width: 6rem;
+                height: 6rem;
+
+                img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: contain;
+                }
+            }
+        }
+
+        .btn-left, .btn-right {
+            position: absolute;
+            left: 0; top: 40%;
+            padding: .3rem .2rem;
+            font-size: 1.5rem;
+        }
+        .btn-right {
+            left: unset;
+            right: 0;
+        }
+    }
+</style>
