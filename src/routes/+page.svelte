d4f36b06791888a1759181b18833d1cc6b3abdc7
Bas de Bruin <bruin998@gmail.com>
2023-11-28 16:22:35 +0100

    start page and button styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,8 +19,8 @@
 <div class="hero">
 
     <img src="/images/logo-accessibility.png" alt="Stichting Accessibility">
-    <h1 style="color: white">Voorbeeldontwerp Webwinkel</h1>
-    <h2 style="color: white">voor senioren</h2>
+    <h1 style="color: white">Preview webwinkel</h1>
+    <h2 style="color: white">voor ouderen</h2>
 
 </div>
 
@@ -29,7 +29,7 @@
         <p>
             In het onderzoek Senioren en Webshops, uitgevoerd door Accessibility in de periode mei 2022 tot juli 2023, is gekeken naar het gebruik van webshops door de oudere online gebruikers (65 jaar en ouder). Het onderzoek bestond uit twee delen: een online enquête onder leden van de ANBO en gebruikersonderzoeken, waarbij oudere deelnmers werden geobserveerd tijdens hjet doen van aankopen in webshops.
         </p>
-        <p>    
+        <p>
             Het doel van dit onderzoek was inzichtelijk maken hoe webshops nog toegankelijker gemaakt kunnen worden voor de oudere doelgroep. Dit is belangrijk, omdat de groep ouderen groeit. Als mesnen ouder worden, krijgen ze te maken met specifieke ouderdomsbeperkingen. Waar moet je als webshop-bouwer of als webshop-eigenaar dan rekening mee houden voor de interessante doelgroep? De inzichten uit het onderzoek zijn verwerkt in dit prototype. Via tooltips kun je de motivatie achter de designkeuzes lezen.
         </p>
         <a href="/home" class="btn">
@@ -60,8 +60,15 @@
 {/if}
 
 <style lang="scss">
+    h1 {
+        font-size: 2rem;
+        margin: 0;
+        margin-top: 1rem;
+    }
     h2 {
+        font-size: 1.5rem;
         font-weight: normal;
+        margin: 0;
     }
     .fixed-page {
         position: fixed;
@@ -76,7 +83,7 @@
         background-color: var(--accessibility-blue);
         color: white;
 
-        padding: 3rem;
+        padding: 2rem 3rem;
 
         img {
             width: 20%;
