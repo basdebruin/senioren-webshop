ecb43fa2213320ad988bc19ff4eb803e223ae9b2
Bas de Bruin <bruin998@gmail.com>
2023-11-28 16:31:31 +0100

    fixed radio input style

--- src/routes/order/TabVerzendmethoden.svelte
+++ src/routes/order/TabVerzendmethoden.svelte
@@ -7,13 +7,12 @@
         { date: "Vr 7 feb", time: "8:30 - 17:00" },
         { date: "Zo 8 feb", time: "8:30 - 17:00" },
     ]
-    let activeDate = 0;
 </script>
 
 <div class="column">
     <h2 class="h4">Verzendgegevens</h2>
     <div class="verzendgegevens">
-        <Tooltip offsetLeft={570} offsetTop={30}>
+        <Tooltip offsetLeft={210} offsetTop={25}>
             <b>Kiezen wanneer en door wie.</b> Ouderen zijn druk, net zoals andere online gebruikers, dus wanneer ze zelf een bezorgmoment kunnen kiezen is dat fijn. Maak je gebruik van verschillende koeriersdiensten? Vermeldt dit erbij. Want uit het onderzoek bleek regelmatig dat oudere bezoekers zo hun voorkeuren voor de een of andere dienst hebben.
         </Tooltip>
         <label class="checkbox">
@@ -22,10 +21,11 @@
         <h3 class="h5"> Kies uw bezorgmoment </h3>
         <div class="date-list">
             {#each dates as date, index}
-                <button class="btn" class:active={activeDate==index} on:click={() => activeDate=index}>
+                <input type="radio" name="date-select" id="date-{index}">
+                <label class="btn" for="date-{index}">
                     <p><b>{date.date}</b></p>
                     <p>{date.time}</p>
-                </button>
+                </label>
             {/each}
         </div>
     </div>
@@ -45,8 +45,23 @@
             display: flex;
             gap: .5rem;
 
-            button.active {
+            label {
+                display: block;
+                width: 100%;
+                padding: 1rem;
+                margin: 0;
+
+                * {
+                    margin: 1rem;
+                }
+            }
+            input:checked + label {
                 background: var(--primary-color) !important;
+                color: white;
+            }
+
+            input {
+                visibility: hidden;
             }
         }
     }
