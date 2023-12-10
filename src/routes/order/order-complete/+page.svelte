f0db02ca93160e0e4749aae4eed78ede6b624ef2
Bas de Bruin <bruin998@gmail.com>
2023-12-10 17:32:36 +0100

    improved order complete page

--- src/routes/order/order-complete/+page.svelte
+++ src/routes/order/order-complete/+page.svelte
@@ -1,6 +1,11 @@
 <script>
     import Tooltip from "$lib/Tooltip/Tooltip.svelte";
+    import { shoppingCartFull } from "$lib/stores";
+    import { onMount } from "svelte";
 
+    onMount(()=> {
+        shoppingCartFull.set(false)
+    })
 </script>
 <div class="spacer" style="height: 100px"></div>
 
@@ -13,15 +18,15 @@
             <Tooltip >
                 <b>Controle en geruststellend.</b> Laat weten dat de bestelling goed is doorgekomen en neem de koper mee in het vervolgproces. Bevestig de afgesproken tijd en het adres opnieuw, dat stelt de klant gerust.
             </Tooltip>
-            <h2 class="h3">Bedankt voor jouw bestelling</h2>
+            <h2>Bedankt voor jouw bestelling</h2>
         
-            <h2 class="h4">Wat gebreurd er nu?</h2>
+            <h3 style="margin-top: 1rem">Wat gebeurt er nu?</h3>
             <ul>
                 <li>Binnen enkele minuten ontvang je een bevestigingsmail op mailadres@datisingevoerd.nl</li>
                 <li>Ondertussen gaan wij aan de slag om jouw bestelling zo snel mogelijk klaar te maken om te versturen.</li>
                 <li>Dit zijn jouw bestelgegevens op een rijtje:</li>
             </ul>
-            <div class="columns">
+            <div class="columns" style="margin: 1.5rem 0 .5rem 0">
                 <p class="column">
                     <b>Bezorgadres</b> <br>
                     Jan de Vries <br>
@@ -40,7 +45,7 @@
 
 
         <div class="column">
-            <h2 class="h3">Naar jou onderweg</h2>
+            <h2>Naar jou onderweg!</h2>
             <div class="product">
                 <div class="columns">
                     <img class="column col-2" src="/images/wasmachines.png" alt="">
@@ -59,6 +64,10 @@
 
 
 <style lang="scss">
+    p, li {
+        max-width: 31em;
+    }
+
     .columns {
         gap: 2rem;
     }
