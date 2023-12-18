a34a2bdcfe41111ff4051853fc1d70523be3b817
Bas de Bruin <bruin998@gmail.com>
2023-12-18 19:06:07 +0100

    name change

--- src/lib/Navigation/Logo.svelte
+++ src/lib/Navigation/Logo.svelte
@@ -1,6 +1,6 @@
 <a href="/home" aria-label="Elektroworld - Ga naar home" id="logo">
     <span class="text">
-        Elektroworld
+        ElektroHub
     </span>
     <svg class="logo-element" width="107" height="117" viewBox="0 0 107 117" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M34.1159 64.4542H0L34.1159 0H58.5H88L68.2319 33.8759H107L34.1159 116.5V64.4542Z" fill="white"/>
@@ -27,7 +27,7 @@
     .logo-element {
         position: absolute;
         top: -2rem;
-        left: 1.5rem;
+        left: 1.2rem;
 
         width: 100px; height: 100px;
 
